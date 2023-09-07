f3991a92c078013baed6b59f12d777238f7684d9
Udayasish <32497837+BANGBANG1999@users.noreply.github.com>
2023-09-07 17:40:17 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = 'mongodb://127.0.0.1:27017/kanbanBoard';
-// const mongoURI = 'mongodb+srv://bangbang:udayasish@projecttaskmanager.5cgfhsl.mongodb.net/kanbanBoard';
+
 
 
 const connectToMongo = async () => {
