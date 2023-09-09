4fab388fcfac305d4ed44c4cf190704342010f65
Udayasish <32497837+BANGBANG1999@users.noreply.github.com>
2023-09-10 02:07:09 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 // const mongoURI = 'mongodb://127.0.0.1:27017/kanbanBoard';
 
-
+const mongoURI = "mongodb+srv://bangbang:udayasish@projecttaskmanager.5cgfhsl.mongodb.net/kanbanBoard"
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
