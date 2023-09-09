928f57ffdccbabdfe87413cdcad53f1d2c31e206
Udayasish <32497837+BANGBANG1999@users.noreply.github.com>
2023-09-10 01:45:26 +0530

    Revert "all Ui changed bro"

--- kanban_board_frontend/src/App.js
+++ kanban_board_frontend/src/App.js
@@ -6,7 +6,6 @@ import Cards from "./components/Cards/Cards";
 import Navbar from "./components/Navbar/Navbar";
 import Topbar from "./components/Topbar/Topbar";
 import TaskState from "./context/tasks/TaskState"
-import MainLayout from "./layout";
 
 
 function App() {
@@ -17,7 +16,8 @@ function App() {
     <DndProvider backend={HTML5Backend}>
         {/* <Navbar /> */}
         <Topbar />
-        <MainLayout/>
+        <Cards />
+        <AllCards />
       </DndProvider>
       </TaskState>
     </>
